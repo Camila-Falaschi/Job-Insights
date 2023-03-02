19d76703cf40926fa09d80d44c53a55581ae6c75
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-03-02 18:49:39 -0300

    :white_check_mark: Chore: Implementing filter_by_salary_range function to filter the jobs by their salary range.

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -106,4 +106,11 @@ def filter_by_salary_range(
     list
         Jobs whose salary range contains `salary`
     """
-    raise NotImplementedError
+    filtered_jobs_list = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered_jobs_list.append(job)
+        except ValueError:
+            pass
+    return filtered_jobs_list
