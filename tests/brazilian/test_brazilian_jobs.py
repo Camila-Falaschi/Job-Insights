7a8639b4383dc5b64bf0cedbb41052a833d19353
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-03-02 20:23:36 -0300

    :white_check_mark: Chore: Implementing test_brazilian_jobs function to verify if read_brazilian_file function is working as expected.

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    dict_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert dict_jobs[1] == {
+        "title": "Motorista",
+        "salary": "3000",
+        "type": "full time",
+    }
